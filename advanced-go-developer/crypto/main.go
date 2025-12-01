28ab9ddd178e31341657dccd6263c22f9f210c4c
Gennady Oleshko <gennady@oleshko-g.ru>
2025-12-01 11:01:59 +0300

    add md5 hashing

--- advanced-go-developer/crypto/main.go
+++ advanced-go-developer/crypto/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
 func main() {
-	fmt.Println(randomBase64String(8))
+	var (
+		data  = make([]byte, 512) // слайс случайных байт
+		hash1 []byte              // хеш с использованием интерфейса hash.Hash
+		hash2 [md5.Size]byte      // хеш, возвращаемый функцией md5.Sum
+	)
+
+	_, _ = rand.Read(data)
+
+	h := md5.New()
+	_, err := h.Write(data)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+	hash1 = h.Sum(hash1)
+	hash2 = md5.Sum(data)
+
+	// hash2[:] приводит массив байт к слайсу
+	if bytes.Equal(hash1, hash2[:]) {
+		fmt.Println("Всё правильно! Хеши равны")
+	} else {
+		fmt.Println("Что-то пошло не так")
+	}
 }
 
 func randomBase64String(l int) string {
